b49497aee4c6c39bf2113e71626f8986d30b571e
Osel_Nicolás <oselko123@gmail.com>
2026-03-20 11:11:25 +0100

    Ejercicio de DOM en Jest

--- Jest 1/task.test.js
+++ Jest 1/task.test.js
@@ -5,7 +5,16 @@ describe('Elementos del DOM', () => {
       <input id="new-task" />
       <button id="add-task">Añadir tarea</button>
       <ul id="task-list"></ul>
-    `
+    `;
+    require('./task.js');
+    
+    const newTask = document.getElementById('new-task');
+    const addButton = document.getElementById('add-task');
+    const taskList = document.getElementById('task-list');
 
-  })
+    newTask.value = 'Nueva tarea';
+    addTask.click();
+
+    expect(taskList.innerHTML).toContain('<li>Nueva tarea</li>')
+  });
 })
