9ef2951bd4072bd42d85d49f09a2220a6eba9228
Osel Nicolás <oselko123@gmail.com>
2026-03-24 11:58:08 +0100

    Add cypress

---
+++ Ejercicios de pdf aprendizaje/Ejercicios2/cypress/e2e/test1/main.cy.js
@@ -0,0 +1,6 @@
+describe('Sintaxis uso Crypress', () => {
+    it('Test comprobación página inicio', () => {
+        cy.visit('http://127.0.0.1:5500/index.html');
+        cy.get('h1').should('have.text', '¡Hola mundo!');
+    })
+})
